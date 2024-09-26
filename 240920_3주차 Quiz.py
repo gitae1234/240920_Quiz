f19ff4fb28702957072ab8fefb55293152883daf
gitae1234 <163506568+gitae1234@users.noreply.github.com>
2024-09-27 00:19:33 +0900

    Add files via upload

---
+++ 240920_3주차 Quiz.py
@@ -0,0 +1,15 @@
+string = 'Hello, World!'
+first_word = string[0:5]
+print(first_word)
+
+result = (7 + 3) ** 2
+print(result)
+
+A = [1, 2, 3, 4, 5]
+result = A[::-1]
+print(result)
+
+
+my_dict = {'apple': 3, 'banana': 5, 'cherry': 2}
+banana_value = my_dict['banana']
+print(banana_value)
